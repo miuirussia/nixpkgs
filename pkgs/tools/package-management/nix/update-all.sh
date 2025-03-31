eff2c80b52d076918eead0b2ca69109161f90d92
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-03-31 14:23:14 +0300

    Cleanup nix script

--- pkgs/tools/package-management/nix/update-all.sh
+++ pkgs/tools/package-management/nix/update-all.sh
@@ -15,7 +15,7 @@ for name in $nix_versions; do
         continue
     fi
 
-    nix-update --override-filename "$SCRIPT_DIR/default.nix" --version-regex "(2\\.${minor_version}\..+)" --build --commit "nixVersions.$name"
+    nix-update --override-filename "$SCRIPT_DIR/default.nix" --version-regex "(2\\.${minor_version}\..+)" --build || true
 done
 
 stable_version_full=$(nix eval --impure --json --expr "with import ./. { config.allowAliases = false; }; nixVersions.stable.version" | jq -r)
@@ -30,9 +30,6 @@ for name in $nix_versions; do
     fi
     if [[ "$name" = "nix_${stable_version_trimmed//./_}" ]]; then
         curl https://releases.nixos.org/nix/nix-$stable_version_full/fallback-paths.nix > "$NIXPKGS_DIR/nixos/modules/installer/tools/nix-fallback-paths.nix"
-        # nix-update will commit the file if it has changed
-        git add "$NIXPKGS_DIR/nixos/modules/installer/tools/nix-fallback-paths.nix"
-        git commit -m "nix: update nix-fallback-paths to $stable_version_full"
         break
     fi
 done
@@ -41,4 +38,4 @@ commit_json=$(curl -s https://api.github.com/repos/NixOS/nix/commits/master) # f
 date_of_commit=$(echo "$commit_json" | jq -r '.commit.author.date')
 suffix="pre$(date -d "$date_of_commit" +%Y%m%d)_"
 sed -i -e "s|\"pre[0-9]\{8\}_|\"$suffix|g" "$SCRIPT_DIR/default.nix"
-nix-update --override-filename "$SCRIPT_DIR/default.nix" --version branch --build --commit "nixVersions.git"
+nix-update --override-filename "$SCRIPT_DIR/default.nix" --version branch --build
